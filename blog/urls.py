cdc65d18121e914511f850a18504df4de4b19046
zdekon <konecny.zdenda@gmail.com>
2020-03-23 16:37:01 +0100

    Added view and template for detailed blog as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),  
+    path('', views.post_list, name='post_list'),
+    path('post/<pk>/', views.post_detail, name='post_detail'),
 ]
